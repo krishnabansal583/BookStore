d4e0ce9e1b243daeeec140f73dc19c091f5386cd
krishna <krishnabansal583@gmail.com>
2024-06-25 01:55:37 +0530

    signup/signin page ready

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../index.css"; 
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="h-screen bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: `url('/popartstyledimageofapackedbookstore-ezgif.com-webp-to-png-converter.png')` }}>
+      <div className="glass-effect rounded-3xl px-6 py-4 w-full md:w-2/5 lg:w-1/3">
+        <p className="text-black text-2xl font-bold text-center">Sign In</p>
 
-export default SignIn
+        <div className="mt-4">
+          <div>
+            <label htmlFor="username" className="text-black font-semibold">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="w-full mt-2 bg-gray-100 text-zinc-800 p-2 outline-none rounded-xl"
+              placeholder="Enter your username"
+              name="username"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="password" className="text-black font-semibold">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full mt-2 bg-gray-100 text-zinc-800 p-2 outline-none rounded-xl"
+              placeholder="Enter your password"
+              name="password"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600"
+            >
+              SignIn
+            </button>
+          </div>
+        </div>
+
+        <p className="mt-4 flex items-center justify-center text-white font-semibold">
+          Or
+        </p>
+
+        <p className="mt-4 flex items-center justify-center text-white font-semibold">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-700 ml-2 hover:underline">
+            SignUp
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../index.css"; 
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="h-screen bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: `url('/popartstyledimageofapackedbookstore-ezgif.com-webp-to-png-converter.png')` }}>
+      <div className="glass-effect rounded-3xl px-6 py-4 w-full md:w-2/5 lg:w-1/3">
+        <p className="text-black text-2xl font-bold text-center">Sign Up</p>
 
-export default SignUp
+        <div className="mt-4">
+          <div>
+            <label htmlFor="username" className="text-black font-semibold">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="w-full mt-2 bg-gray-100 text-zinc-800 p-2 outline-none rounded-xl"
+              placeholder="Enter your username"
+              name="username"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="email" className="text-black font-semibold">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full mt-2 bg-gray-100 text-zinc-800 p-2 outline-none rounded-xl"
+              placeholder="xyz@example.com"
+              name="email"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="password" className="text-black font-semibold">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full mt-2 bg-gray-100 text-zinc-800 p-2 outline-none rounded-xl"
+              placeholder="Enter your password"
+              name="password"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="address" className="text-black font-semibold">
+              Address
+            </label>
+            <textarea
+              id="address"
+              className="w-full mt-2 bg-gray-100 text-zinc-800 p-2 outline-none rounded-xl"
+              rows="3"
+              placeholder="Enter your address"
+              name="address"
+              required
+            />
+          </div>
+
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white font-semibold py-2 rounded hover:bg-blue-600"
+            >
+              SignUp
+            </button>
+          </div>
+        </div>
+
+        <p className="mt-4 flex items-center justify-center text-white font-semibold">
+          Or
+        </p>
+
+        <p className="mt-4 flex items-center justify-center text-white font-semibold">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-700 ml-2 hover:underline">
+            SignIn
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
