3d2f71a5a77bd84f3eca33f580f0438cbab322bf
krishna <krishnabansal583@gmail.com>
2024-06-24 14:19:35 +0530

    all-books display

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../components/Loader/Loader";
+import BookCart from "../components/BookCart/BookCart";
 
 const AllBooks = () => {
-  return (
-    <div>AllBooks</div>
-  )
-}
+  const [Data, setData] = useState();
 
-export default AllBooks
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(
+        "http://localhost:1000/api/v1/get-all-books"
+      );
+      setData(response.data.data);
+    };
+    fetch();
+  }, []);
+
+  return (
+    <div className="gradient-background px-12 h-auto py-8">
+      <h2 className="text-4xl font-semibold text-black mb-4">All Books</h2>
+      {!Data && (
+        <div className="flex items-center justify-center my-8">
+          <Loader />
+        </div>
+      )}
+      <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8">
+        {Data &&
+          Data.map((items, i) => (
+            <div key={i}>
+              <BookCart data={items} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllBooks;
